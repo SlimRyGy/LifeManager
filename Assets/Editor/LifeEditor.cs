e90215a989f8ea89490873b97e4069a3912eb913
Rygy <remy.grandry95@gmail.com>
2017-12-18 12:33:41 +0100

    Editor Button

---
+++ Assets/Editor/LifeEditor.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LifeManager))]
+public class LifeEditor : Editor 
+{
+    GUIStyle m_labelStyle = new GUIStyle();
+    GUIStyle m_rezButtonStyle = new GUIStyle();
+    GUIStyle m_dieButtonStyle = new GUIStyle();
+
+    public override void OnInspectorGUI()
+    {
+        m_labelStyle.alignment              = TextAnchor.MiddleCenter;
+        m_labelStyle.fontStyle              = FontStyle.Bold;
+        m_labelStyle.normal.textColor       = Color.blue;
+
+        m_dieButtonStyle.normal.textColor   = Color.red;
+        m_rezButtonStyle.normal.textColor   = Color.green;
+        m_dieButtonStyle.alignment          = TextAnchor.MiddleLeft;
+        m_rezButtonStyle.alignment          = TextAnchor.MiddleRight;
+        m_dieButtonStyle.padding.left       = 170;
+        m_rezButtonStyle.padding.right      = 170;
+
+        LifeManager m_myLifeManager = (LifeManager)target;
+
+        DrawDefaultInspector();
+
+        GUILayout.Label("");
+        GUILayout.Label("Interact with life", m_labelStyle);
+
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("Kill it", m_dieButtonStyle))
+        {
+            Debug.Log("Kill it");
+            m_myLifeManager.Life = 0f;
+        }
+        if (GUILayout.Button("Resurection", m_rezButtonStyle))
+        {
+            Debug.Log("Resurection");
+            m_myLifeManager.Life = 1f;
+        }
+        GUILayout.EndHorizontal();
+        GUILayout.Label("");
+
+    }
+}
---
+++ Assets/Scripts/LifeManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class LifeManager : MonoBehaviour 
+{
+    [Range(0, 1f)]
+    [SerializeField]
+    [Header("Life")]
+    [Tooltip("Life of the target between 0-1 percent")]
+    /// Life of the target between 0-1 percent.
+                
+    private float m_life;
+
+    public UnityEvent m_onDeath;
+
+    public float Life         
+    {
+        get { return m_life; }
+        set
+        {
+            value = Mathf.Clamp01(value);
+            m_life = value;
+        }
+    }
+
+    [Header("Debug")]
+    public bool m_useDebugLog;
+
+    #region System
+
+    private void OnValidate()
+    {
+        
+    }
+
+    private void Reset()
+    {
+        m_life = 0f;
+    }
+
+    #endregion
+
+}
